ce55e36c2786cfb26714e0b2a376d1dc6d5d88f0
pedrops164 <A95826@alunos.uminho.pt>
2023-11-27 20:38:23 +0000

    added base code to competition prediction

---
+++ competicao/src/model.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from preprocess import *
+
+# Load input csv (energia and meteo)
+df_energia_2021 = pd.read_csv('../input/energia_202109-202112.csv', encoding='latin-1', na_values=["", "NaN", ""])
+df_energia_2022 = pd.read_csv('../input/energia_202201-202212.csv', encoding='latin-1', na_values=["", "NaN", ""])
+df_meteo_2021 = pd.read_csv('../input/meteo_202109-202112.csv', encoding='latin-1', na_values=["", "NaN", ""])
+df_meteo_2022 = pd.read_csv('../input/meteo_202201-202212.csv', encoding='latin-1', na_values=["", "NaN", ""])
+
+train_energia = pd.concat([df_energia_2021, df_energia_2022])
+train_meteo = pd.concat([df_meteo_2021, df_meteo_2022])
+
+test_energia_X = pd.read_csv('../input/energia_202301-202304.csv', encoding='latin-1')
+test_meteo_X = pd.read_csv('../input/meteo_202301-202304.csv', encoding='latin-1')
+
+# Prepressing energia dataset
+preprocess_data(train_energia, test_energia_X)
+preprocess_hora(train_energia, test_energia_X)
+preprocess_normal(train_energia, test_energia_X)
+preprocess_horario(train_energia, test_energia_X)
+preprocess_autoconsumo(train_energia, test_energia_X)
+preprocess_injecao(train_energia, test_energia_X)
+
+# Prepressing meteo dataset
+preprocess_dt(train_meteo, test_meteo_X)
+preprocess_dtiso(train_meteo, test_meteo_X)
+preprocess_cityname(train_meteo, test_meteo_X)
+preprocess_temp(train_meteo, test_meteo_X)
+preprocess_feelslike(train_meteo, test_meteo_X)
+preprocess_temp_min(train_meteo, test_meteo_X)
+preprocess_temp_max(train_meteo, test_meteo_X)
+preprocess_pressure(train_meteo, test_meteo_X)
+preprocess_sea_level(train_meteo, test_meteo_X)
+preprocess_grnd_level(train_meteo, test_meteo_X)
+preprocess_humidity(train_meteo, test_meteo_X)
+preprocess_wind_speed(train_meteo, test_meteo_X)
+preprocess_rain1h(train_meteo, test_meteo_X)
+preprocess_clouds_all(train_meteo, test_meteo_X)
+preprocess_weather_description(train_meteo, test_meteo_X)
---
+++ competicao/src/preprocess.py
@@ -0,0 +1,72 @@
+import pandas as pd
+
+# PREPROCESSAMENTO ENERGIA
+
+def preprocess_data(train, test_X):
+    pass
+
+def preprocess_hora(train, test_X):
+    pass
+
+def preprocess_normal(train, test_X):
+    pass
+
+def preprocess_horario(train, test_X):
+    # horario economico
+    pass
+
+def preprocess_autoconsumo(train, test_X):
+    pass
+
+def preprocess_injecao(train, test_X):
+    # injecao na rede
+    pass
+
+
+# PREPROCESSAMENTO METEO
+
+def preprocess_dt(train, test_X):
+    pass
+
+def preprocess_dtiso(train, test_X):
+    pass
+
+def preprocess_cityname(train, test_X):
+    pass
+
+def preprocess_temp(train, test_X):
+    pass
+
+def preprocess_feelslike(train, test_X):
+    pass
+
+def preprocess_temp_min(train, test_X):
+    pass
+
+def preprocess_temp_max(train, test_X):
+    pass
+
+def preprocess_pressure(train, test_X):
+    pass
+
+def preprocess_sea_level(train, test_X):
+    pass
+
+def preprocess_grnd_level(train, test_X):
+    pass
+
+def preprocess_humidity(train, test_X):
+    pass
+
+def preprocess_wind_speed(train, test_X):
+    pass
+
+def preprocess_rain1h(train, test_X):
+    pass
+
+def preprocess_clouds_all(train, test_X):
+    pass
+
+def preprocess_weather_description(train, test_X):
+    pass
+
